b7e8e1be27e9b3d77aea724864748c4a92ea0778
Jai Juneja <46759278+jaiongit@users.noreply.github.com>
2021-12-04 22:49:18 +0530

    Save issue links to PDF instead of JSON

--- issue_page.js
+++ issue_page.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
+const PDF = require('pdfkit');
 const homeURL = 'https://github.com';
 
 function extract_issue_links(html, topicDirectory, repoName) {
@@ -17,8 +18,11 @@ function extract_issue_links(html, topicDirectory, repoName) {
     }
 
     // write to a repo named json file
-    let fileLoc = path.join(topicDirectory, repoName + '.json');
-    fs.writeFileSync(fileLoc, JSON.stringify(issueLinksArr));
+    let fileLoc = path.join(topicDirectory, repoName + '.pdf');
+    let pdfDoc = new PDF;
+    pdfDoc.pipe(fs.createWriteStream(fileLoc));
+    pdfDoc.text(issueLinksArr.join('\n'));
+    pdfDoc.end();
 }
 
 module.exports = extract_issue_links;
